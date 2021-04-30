1bfeeab0f2fc175afbcdbefe7e5b44abe718b267
MobileMars76874 <72148112+MobileMars76874@users.noreply.github.com>
2021-04-30 17:32:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+left_wrist_x = 0;
+right_wrist_x = 0;
+Wrist_d = 0;
+
+function preload(){
+}
+
+function setup(){
+canvas = createCanvas(400, 400);
+canvas.center();
+Video = createCapture(VIDEO);
+Video.hide();
+
+poseNet = ml5.poseNet(Video, model_loaded);
+poseNet.on("pose", gopose);
+}
+
+function model_loaded(){
+console.log("Model Loaded!");
+}
+
+function draw(){
+textSize(Wrist_d);
+fill("#00ffff");
+text("Hello", 50, 200);
+}
+
+function gopose(results){
+if(results.length > 0){
+console.log(results);
+
+left_wrist_x = results[0].pose.leftWrist.x;
+right_wrist_x = results[0].pose.rightWrist.x;
+
+Wrist_d = floor(left_wrist_x - right_wrist_x);
+}
+}
